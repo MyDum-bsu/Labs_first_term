6071bed3e02c27b8760b5c56926aea50450adcdb
mydum <fpm.mayorov@bsu.by>
2021-10-18 15:57:45 +0300

    lab4: A&B

---
+++ lab4.8A.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+
+using namespace std;
+
+void CinString(char *string) {
+    int n;
+    cout << "Enter the length of the string" << endl;
+    cin >> n;
+    cout << "Enter your string" << endl;
+    for (int i = 0; i < n; ++i)
+        cin >> string[i];
+}
+
+char *strrchr1(char *string, int symbol) {
+    int i;
+    for (i = 0; string[i] != '\0'; i++) {}
+    for (int j = i - 1; j >= 0; j--) {
+        if (string[j] == symbol) {
+            cout << "Index of the entered symbol - " << j << endl;
+            return (string + j);
+        }
+        if (string[j] != symbol && j == 0) return nullptr;
+    }
+
+}
+
+int main() {
+    char *string;
+    CinString(string);
+    char symbol;
+    cout << "Enter the symbol to search for" << endl;
+    cin >> symbol;
+    cout << strrchr1(string, symbol)[0];
+    return 0;
+}
---
+++ lab4.8B.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+
+using namespace std;
+
+void CinString(char *string, int size) {
+    cout << "Enter your string" << endl;
+    cin.getline(string, size + 1);
+    cin.getline(string, size + 1);
+}
+
+bool UniqueWordDeterminant(char *begin, const char *end) {
+    for (char *i = begin; i <= end; ++i) {
+        for (char *j = begin; j < i; ++j) {
+            if (*i == *j)
+                return false;
+        }
+    }
+    return true;
+}
+
+void SearchWord(char *string, int n) {
+    char *t_end;
+    char *t_begin = string;
+    char *end = string;
+    char *begin = string + 1;
+    for (int i = 0; i < n; ++i) {
+        if (string[i] == ' ' || string[i] == '\0') {
+            t_end = string + i - 1;
+            if ((t_end >= t_begin) && (UniqueWordDeterminant(t_begin, t_end))) {
+                end = t_end;
+                begin = t_begin;
+                break;
+            }
+            t_begin = string + i + 1;
+        }
+    }
+
+    if (end >= begin) {
+        for (char *i = begin; i <= end; ++i) {
+            cout << *i;
+        }
+    } else
+        cout << "NO UNIQUE WORDS" << endl;
+}
+
+int main() {
+    int n;
+    cout << "Enter the length of the string" << endl;
+    cin >> n;
+    char *string = new char[n + 1];
+    CinString(string, n);
+    SearchWord(string, n + 1);
+    delete[] string;
+    return 0;
+}
